e50ec5a8e771627b570bc4f5e69c2cd92fb4753e
Y Eswar Aditya <yeswaraditya3006@gmail.com>
2023-12-22 09:54:21 +0530

    ja

--- first.js
+++ first.js
@@ -1,8 +1,20 @@
-const product = {
+   /* const product = {
     title : "Ball pen",
     rating : 4,
     offer: 5,
     price : 270,
 
 
-};
+    };*/
+
+
+    const linkedin = {
+
+        username : "shar",
+        posts: 322,
+        followers:2323,
+        following:2223,
+        details: "dfdwffdfdfdfdfdwf df df df d f adf f",
+
+    };
+    document.write(linkedin["posts"]);
