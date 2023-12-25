7eccd7a837f4754148fa420094301bc61b95fce3
Y Eswar Aditya <yeswaraditya3006@gmail.com>
2023-12-25 12:48:34 +0530

    practice question4

--- first.js
+++ first.js
@@ -1,4 +1,6 @@
-    /*const product = {
+   
+   //practice Question 1
+   /*const product = {
     title : "Ball pen",
     rating : 4,
     offer: 5,
@@ -7,7 +9,7 @@
 
     };*/
 
-
+ //practice Question 2
     /*const linkedin = {
 
         username : "shar",
@@ -19,14 +21,42 @@
     };
     document.write(linkedin["posts"]);*/
 
+ //practice Question 3
+    /*let number=prompt("enter a number: ");
 
-    let number=prompt("enter a number: ");
-
-    if(number%5==0)
+    if(number%5===0)
     {
         console.log("is multiple of 5");
     }
     else
     {
         console.log("not a multiple of 5");
-    }
+    }*/
+
+
+ //practice Question 4
+
+/*let marks=prompt("enter score: ");
+
+if ( marks>=90 && marks <=100)
+{
+    console.log("greade is A");
+}
+
+else if (marks>=70 && marks<=89)
+{
+    console.log("grade is B");
+}
+else if (marks>=60 && marks<=69)
+{
+    console.log("grade is C");
+}
+else if (marks>=50 && marks<=59)
+{
+    console.log("grade is D");
+}
+else if (marks>=0&& marks<=49)
+{
+    console.log("grade is F");
+}*/
+
