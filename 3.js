da6b374945f6b751e9a83281c62cce2e706585a4
Y Eswar Aditya <yeswaraditya3006@gmail.com>
2023-12-31 11:22:45 +0530

    js

---
+++ 3.js
@@ -0,0 +1,15 @@
+//strings
+
+// let str1="hello";
+// let str2="hello";
+
+// document.write(str1[1]); // e
+ 
+
+
+
+// Template literals
+
+let templit=`this is template literal`;
+
+document.write(typeof templit);
